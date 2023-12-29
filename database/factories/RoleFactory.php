14f72f2ed017725ddeeb8f79cf316488a9a66374
Abel Shibabaw <abelshibabaw291@gmail.com>
2023-12-29 15:10:21 +0300

    added folder structure

--- database/factories/RoleFactory.php
+++ database/factories/RoleFactory.php
@@ -17,7 +17,9 @@ class RoleFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->unique()->word, // Unique role names
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 }
